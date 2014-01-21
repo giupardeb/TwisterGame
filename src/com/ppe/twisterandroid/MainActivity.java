427e68cbcc68fa4842c319d8b055115e3bf12d8e
giupardeb <giuseppeparasiliti93@gmail.com>
2014-01-21 09:57:34 +0100

    Aggiornamento prima Activity

--- src/com/ppe/twisterandroid/MainActivity.java
+++ src/com/ppe/twisterandroid/MainActivity.java
@@ -1,15 +1,56 @@
 package com.ppe.twisterandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	private String m_Text = "";
+	private Button SceltaGiocatori;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        SceltaGiocatori = (Button)findViewById(R.id.buttonGiocatori);
+        SceltaGiocatori.setOnClickListener(new OnClickListener(){
+        	@Override
+			public void onClick(View v) {
+        		
+        		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        		builder.setTitle("Quanti Giocatori Siete? (max 6)");
+
+        		// Set up the input
+        		final EditText input = new EditText(MainActivity.this);
+        		// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        		input.setInputType(InputType.TYPE_CLASS_NUMBER| InputType.TYPE_NUMBER_FLAG_SIGNED);
+        		builder.setView(input);
+
+        		// Set up the buttons
+        		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() { 
+        		    @Override
+        		    public void onClick(DialogInterface dialog, int which) {
+        		        m_Text = input.getText().toString();
+        		    }
+        		});
+        		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        		    @Override
+        		    public void onClick(DialogInterface dialog, int which) {
+        		        dialog.cancel();
+        		    }
+        		});
+
+        		builder.show();
+        		
+			}
+        });
     }
 
 
